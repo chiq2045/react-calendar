a105724d0ba1557b31b5f79d56fbb6579dcbd6fe
chiq2045 <charles.oroko@outlook.com>
2020-12-14 23:43:11 +0300

    add calendar header

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import dayjs from 'dayjs';
 
 export default function Calendar () {
   return (
-    <div>This is the Calendar</div>
+    <div>
+      <div>
+        Calendar
+      </div>
+      <div>
+        <button className='btn-transparent'>
+          <i className='fa-wrapper fa fa-chevron-left' />
+        </button>
+        <div>
+          {dayjs().format('MMMM')}
+        </div>
+        <div>
+          {dayjs().format('YYYY')}
+        </div>
+        <button className='btn-transparent'>
+          <i className='fa-wrapper fa fa-chevron-right' />
+        </button>
+      </div>
+    </div>
   );
 }
