12ac3d82e4ab58fae1cceb2a825c7e8e970fba5d
chiq2045 <charles.oroko@outlook.com>
2020-12-14 23:44:35 +0300

    create modules for header

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -2,25 +2,37 @@ import React from 'react';
 import dayjs from 'dayjs';
 
 export default function Calendar () {
-  return (
-    <div>
-      <div>
-        Calendar
-      </div>
+  const RenderCalendarHeader = displayDate => {
+    return (
       <div>
         <button className='btn-transparent'>
           <i className='fa-wrapper fa fa-chevron-left' />
         </button>
         <div>
-          {dayjs().format('MMMM')}
+          {displayDate().format('MMMM')}
         </div>
         <div>
-          {dayjs().format('YYYY')}
+          {displayDate().format('YYYY')}
         </div>
         <button className='btn-transparent'>
           <i className='fa-wrapper fa fa-chevron-right' />
         </button>
       </div>
+    );
+  };
+
+  const RenderCalendarTitle = title => {
+    return (
+      <div>
+        {title}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {RenderCalendarTitle('Test Calendar')}
+      {RenderCalendarHeader(dayjs)}
     </div>
   );
 }
